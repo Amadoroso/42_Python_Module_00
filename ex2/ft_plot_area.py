6b65489d85711cce176fe52b295984501b8b607b
André Amador <andreamador@Mac.lan>
2026-07-01 19:10:04 +0100

    ex3

--- ex2/ft_plot_area.py
+++ ex2/ft_plot_area.py
@@ -1,5 +1,5 @@
 
 def ft_plot_area():
-    lenght : int = int(input("Enter length: "))
-    width : int = int(input("Enter width: "))
+    lenght: int = int(input("Enter length: "))
+    width: int = int(input("Enter width: "))
     print(f"Plot area: {lenght * width}")
--- ex3/ft_harvest_total.py
+++ ex3/ft_harvest_total.py
@@ -1,1 +1,7 @@
-
+# O for é tipo um while com o iterador integrado
+def ft_harvest_total():
+    harvests: list = []
+    for day in range(1, 4):  # neste caso range seria {1,2,3}
+        day_amount: int = int(input(f"Day {day} harvest: "))
+        harvests.append(day_amount)
+    print(f"Total harvest: {sum(harvests)}")
